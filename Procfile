5f5797fa6aef63bb5050cf404e2df536be885d3e
Tomonobu Fukuhara <fukchr@gmail.com>
2024-09-26 10:31:50 -0400

    comment scheduler for now

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 web: gunicorn gettingstarted.wsgi
-scheduler: python manage.py run_scheduler  # Custom command to start the scheduler
+# scheduler: python manage.py run_scheduler  # Custom command to start the scheduler
 
 # Uncomment this `release` process if you are using a database, so that Django's model
 # migrations are run as part of app deployment, using Heroku's Release Phase feature:
--- hello/views.py
+++ hello/views.py
@@ -263,8 +263,8 @@ class OhipBulletinAPIView(APIView):
             # START Testing AREA
             response_text = "empty first"
             cached_data = cache.get("bulletin")
-            # if cached_data:
-            #     return Response(cached_data, status=status.HTTP_200_OK)
+            if cached_data:
+                return Response(cached_data, status=status.HTTP_200_OK)
             bulletin_cache = {
                 "bulletinInfo": response_text,
                 "timestamp" : datetime.now()
